890411493d708a8e8bb1b2f06a1a56a22b2ffae5
nadirbasalamah <nadir008basalamah@gmail.com>
2022-10-05 12:36:41 +0700

    feat(test): add additional unit tests for category

--- businesses/categories/usecase_test.go
+++ businesses/categories/usecase_test.go
@@ -20,6 +20,8 @@ func TestMain(m *testing.M) {
 	categoryDomain = categories.Domain{
 		Name: "test",
 	}
+
+	m.Run()
 }
 
 func TestGetAll(t *testing.T) {
@@ -28,7 +30,7 @@ func TestGetAll(t *testing.T) {
 
 		result := categoryService.GetAll()
 
-		assert.Equal(t, 0, len(result))
+		assert.Equal(t, 1, len(result))
 	})
 
 	t.Run("Get All | InValid", func(t *testing.T) {
@@ -37,5 +39,77 @@ func TestGetAll(t *testing.T) {
 		result := categoryService.GetAll()
 
 		assert.Equal(t, 0, len(result))
+	})
+}
+
+func TestGetByID(t *testing.T) {
+	t.Run("Get By ID | Valid", func(t *testing.T) {
+		categoryRepository.On("GetByID", "1").Return(categoryDomain).Once()
+
+		result := categoryService.GetByID("1")
+
+		assert.NotNil(t, result)
+	})
+
+	t.Run("Get By ID | InValid", func(t *testing.T) {
+		categoryRepository.On("GetByID", "-1").Return(categories.Domain{}).Once()
+
+		result := categoryService.GetByID("-1")
+
+		assert.NotNil(t, result)
+	})
+}
+
+func TestCreate(t *testing.T) {
+	t.Run("Create | Valid", func(t *testing.T) {
+		categoryRepository.On("Create", &categoryDomain).Return(categoryDomain).Once()
+
+		result := categoryService.Create(&categoryDomain)
+
+		assert.NotNil(t, result)
+	})
+
+	t.Run("Create | InValid", func(t *testing.T) {
+		categoryRepository.On("Create", &categories.Domain{}).Return(categories.Domain{}).Once()
+
+		result := categoryService.Create(&categories.Domain{})
+
+		assert.NotNil(t, result)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("Update | Valid", func(t *testing.T) {
+		categoryRepository.On("Update", "1", &categoryDomain).Return(categoryDomain).Once()
+
+		result := categoryService.Update("1", &categoryDomain)
+
+		assert.NotNil(t, result)
+	})
+
+	t.Run("Update | InValid", func(t *testing.T) {
+		categoryRepository.On("Update", "1", &categories.Domain{}).Return(categories.Domain{}).Once()
+
+		result := categoryService.Update("1", &categories.Domain{})
+
+		assert.NotNil(t, result)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("Delete | Valid", func(t *testing.T) {
+		categoryRepository.On("Delete", "1").Return(true).Once()
+
+		result := categoryService.Delete("1")
+
+		assert.True(t, result)
+	})
+
+	t.Run("Delete | InValid", func(t *testing.T) {
+		categoryRepository.On("Delete", "-1").Return(false).Once()
+
+		result := categoryService.Delete("-1")
+
+		assert.False(t, result)
 	})
 }
